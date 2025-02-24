5652d372dbc389a7006a2c51fccfd638f09401a3
digitalprateek <119813760+digitalprateek@users.noreply.github.com>
2025-02-24 12:33:36 +0530

    Delete src directory to add new

--- src/configs/jwtConfig.js
+++
@@ -1,8 +0,0 @@
-
-//this has to be a secret key that only the server knows and hide it like and shouldn't be hardcoded
-
-const jwtSecretKey = process.env.jwtSecretKey;
-
-module.exports = {
-    jwtSecretKey
-};
